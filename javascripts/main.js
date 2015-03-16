9ec70fc0a533ec6c4142b17352568e5c5668da4c
Jayson Virissimo <jayson.virissimo@asu.edu>
2015-03-16 13:11:59 -0700

    fix easy rule generator

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,8 @@
 var firstClause = "A Koan has the Buddha-nature if and only if ";
 var colors = ["yellow", "black", "green", "blue"];
 var sizes = ["small", "medium", "large"];
+var quantities = ["two", "three"];
+var orientations = ["grounded", "ungrounded", "upright", "sideways"];
 
 
 function randomSelection (array) {
@@ -9,20 +11,23 @@ function randomSelection (array) {
 
 function easyRule () {
   var rule = firstClause;
-  var number = randomSelection([0, 1, 2, 3])
+  var number = randomSelection([0, 1, 2, 3, 4])
 
   switch(number) {
     case 0:
-      rule += "it has one or more " + randomSelection(colors) + " pieces."
+      rule += "it has " + randomSelection(quantities) + " or more " + randomSelection(colors) + " pieces."
       break;
     case 1:
-      rule += "it has exactly one " + randomSelection(colors) + " piece."
+      rule += "is has exactly one " + randomSelection(sizes) + " " + randomSelection(orientations) + " " + randomSelection(colors) + " piece."
       break;
     case 2:
-      rule += "it has one or more " + randomSelection(sizes) + " " + randomSelection(colors) + " pieces."
+      rule += "it has exactly two " + randomSelection(sizes) + " " + randomSelection(colors) + " pieces."
       break;
     case 3:
-      rule += "it has exactly one " + randomSelection(sizes) + " " + randomSelection(colors) + " piece."
+      rule += "is has at least one " + randomSelection(colors) + " piece pointing at a " + randomSelection(colors) + " piece."
+      break;
+    case 4:
+      rule += "is has exactly one " + randomSelection(colors) + " piece and exactly one " + randomSelection(sizes) + " piece."
       break;
   }
 
