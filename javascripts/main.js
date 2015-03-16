d2a70eacc28fe79c91216807dcfeb756e5aa7d58
Jayson Virissimo <jayson.virissimo@asu.edu>
2015-03-16 12:13:16 -0700

    create basic easy rule generator

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,31 @@
+var firstClause = "A Koan has the Buddha-nature if and only if ";
+var colors = ["yellow", "black", "green", "blue"];
+var sizes = ["small", "medium", "large"];
+
+
 function randomSelection (array) {
   return array[Math.floor(Math.random()*array.length)];
+}
+
+function easyRule () {
+  var rule = firstClause;
+  var number = randomSelection([0, 1, 2, 3])
+
+  switch(number) {
+    case 0:
+      rule += "it has one or more " + randomSelection(colors) + " pieces."
+      break;
+    case 1:
+      rule += "it has exactly one " + randomSelection(colors) + " piece."
+      break;
+    case 2:
+      rule += "it has one or more " + randomSelection(sizes) + " " + randomSelection(colors) + " pieces."
+      break;
+    case 3:
+      rule += "it has exactly one " + randomSelection(sizes) + " " + randomSelection(colors) + " piece."
+      break;
+  }
+
+
+  return rule;
 }
