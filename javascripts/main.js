c2e7e568777beb521c56e79f75093cffe608ece2
Jayson Virissimo <jayson.virissimo@asu.edu>
2015-03-16 22:52:22 -0700

    expand variability of easy rule generator

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ var firstClause = "A Koan has the Buddha-nature if and only if ";
 var colors = ["yellow", "black", "green", "blue"];
 var sizes = ["small", "medium", "large"];
 var quantities = ["two", "three"];
-var orientations = ["grounded", "ungrounded", "upright", "sideways"];
+var orientations = ["grounded", "ungrounded", "upright", "flat"];
 
 
 function randomSelection (array) {
@@ -11,23 +11,38 @@ function randomSelection (array) {
 
 function easyRule () {
   var rule = firstClause;
-  var number = randomSelection([0, 1, 2, 3, 4])
+  var number = randomSelection([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
   switch(number) {
     case 0:
-      rule += "it has " + randomSelection(quantities) + " or more " + randomSelection(colors) + " pieces."
+      rule += "it has " + randomSelection(quantities) + " or more " + randomSelection(colors) + " pieces.";
       break;
     case 1:
-      rule += "is has exactly one " + randomSelection(sizes) + " " + randomSelection(orientations) + " " + randomSelection(colors) + " piece."
+      rule += "is has exactly one " + randomSelection(sizes) + " " + randomSelection(orientations) + " " + randomSelection(colors) + " piece.";
       break;
     case 2:
-      rule += "it has exactly two " + randomSelection(sizes) + " " + randomSelection(colors) + " pieces."
+      rule += "it has exactly two " + randomSelection(sizes) + " " + randomSelection(colors) + " pieces.";
       break;
     case 3:
-      rule += "is has at least one " + randomSelection(colors) + " piece pointing at a " + randomSelection(colors) + " piece."
+      rule += "is has at least one " + randomSelection(colors) + " piece pointing at a " + randomSelection(colors) + " piece.";
       break;
     case 4:
-      rule += "is has exactly one " + randomSelection(colors) + " piece and exactly one " + randomSelection(sizes) + " piece."
+      rule += "is has exactly one " + randomSelection(colors) + " piece and exactly one " + randomSelection(sizes) + " piece.";
+      break;
+    case 5:
+      rule += "it has an equal number of " + randomSelection(colors) + " and " + randomSelection(sizes) + " pieces.";
+      break;
+    case 6:
+      rule += "all flat pieces point at a " + randomSelection(sizes) + " piece.";
+      break;
+    case 7:
+      rule += "all of the " + randomSelection(colors) + " pieces are " + randomSelection(orientations) + ".";
+      break;
+    case 8:
+      rule += "all of the pieces are " + randomSelection(orientations) + ", except for the " + randomSelection(colors) + " pieces.";
+      break;
+    case 9:
+      rule += "all of the pieces are " + randomSelection(orientations) + ", except for the " + randomSelection(sizes) + " pieces."
       break;
   }
 
