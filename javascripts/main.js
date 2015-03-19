992ac57758be2df95b5276e1ab04721661352849
Jayson Virissimo <jayson.virissimo@asu.edu>
2015-03-18 22:04:05 -0700

    add medium rule function

--- javascripts/main.js
+++ javascripts/main.js
@@ -46,6 +46,47 @@ function easyRule () {
       break;
   }
 
+  return rule;
+}
+
+function mediumRule () {
+  var rule = firstClause;
+  var number = randomSelection([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+  switch(number) {
+    case 0:
+      rule += "all flat pieces point at " + randomSelection(colors) + " pieces, and there exists at least one flat piece.";
+      break;
+    case 1:
+      var color = randomSelection(colors);
+      rule += "all of the " + color + " pieces are " + randomSelection(orientations) + ", and there exists at least one " + color + " piece.";
+      break;
+    case 2:
+      rule += "all the pieces are " + randomSelection(orientations) + ", except for " + randomSelection(colors) + " pieces.";
+      break;
+    case 3:
+      rule += "all the small pieces are " + randomSelection(colors) + ", all the medium pieces are " + randomSelection(colors) + ", and all the large pieces are " + randomSelection(colors) + ".";
+      break;
+    case 4:
+      rule += "all the " + randomSelection(orientations) + " pieces are the same size.";
+      break;
+    case 5:
+      rule += "all the " + randomSelection(colors) + " pieces are the same size.";;
+      break;
+    case 6:
+      rule += "exactly half of the pieces are " + randomSelection(sizes) + ".";
+      break;
+    case 7:
+      rule += "a " + randomSelection(sizes) + " piece touches a " + randomSelection(orientations) + " piece.";
+      break;
+    case 8:
+      var orientation = randomSelection(orientations);
+      rule += "all of the " + orientation + " pieces are " + randomSelection(sizes) + ", and there exists at least one " + orientation + " piece.";
+      break;
+    case 9:
+      rule += "it has a " + randomSelection(orientations) + " piece, but no " + randomSelection(colors) + " pieces.";
+      break;
+  }
 
   return rule;
 }
